6fbe1eec86425f02429873e30d5ea001ffd93372
tuongtacfree <wny9930@gmail.com>
2026-09-10 19:26:30 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// CHỈ điền 2 giá trị public của project Supabase.
-// Không đưa service_role key vào GitHub Pages.
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY";
-const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
