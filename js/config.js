7c234b09df954ccde899e78bd847093de708983f
tuongtacfree <wny9930@gmail.com>
2026-09-10 19:48:48 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // CHỈ điền 2 giá trị public của project Supabase.
 // Không đưa service_role key vào GitHub Pages.
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://slryfjxvcnjrfhobgqze.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_z1AJNNfbfSS7IN5s_bEJPw_jo6LaaZs";
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
